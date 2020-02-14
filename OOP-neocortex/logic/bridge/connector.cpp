dcf71119308fce439b220a5b336890eb831de57e
MilesArtemius <shveitsar215@gmail.com>
2020-02-14 10:58:49 +0300

    Update connector.cpp

--- OOP-neocortex/logic/bridge/connector.cpp
+++ OOP-neocortex/logic/bridge/connector.cpp
@@ -3,21 +3,26 @@
 #include "../operational/game.h"
 
 
-void init(int seed) {
+// is called when game is initialized;
+extern "C" const char * on_start(int seed) {
     std::shared_ptr<game> g = game::get(seed);
     log::say("Game initialized.");
-    return;
+    return (new std::string())->data();
 }
 
-void update() {
+// is called on every update loop iteration;
+extern "C" const char * on_update() {
     log::say("State updated.");
-    return;
+    return (new std::string())->data();
 }
 
-void connect(command signal) {
-    log::say("Signal received.");
+// is called on every user command, including save/load;
+extern "C" const char * on_signal(const char* json_input) {
+    log::say("State updated.");
+    return (new std::string())->data();
 }
 
-void destroy() {
+// is called when game session finished;
+extern "C" void on_destroy() {
     log::say("Game terminated.");
 }
