54f602971f23593320fc525abec28981346eb94d
milesartemius <shveitsar215@gmail.com>
2021-09-25 18:25:08 +0300

    jupyter?

--- sem7/fix_notebooks.py
+++ sem7/fix_notebooks.py
@@ -4,11 +4,13 @@ import json
 
 
 # The script fixes code cell numbers in .ipynb files for them to start from 1. Yes it IS important.
-for notebook in [ path for path in Path(".").rglob("[!venv]*.ipynb") if not any(x in realpath(path) for x in ("venv", ".ipynb_checkpoints", "__pycache__")) ]:
-    parsed = json.loads(notebook.read_text())
+for notebook in [ path for path in Path(".").rglob("*.ipynb") if not any(x in realpath(path) for x in ("venv", ".ipynb_checkpoints", "__pycache__")) ]:
+    parsed = json.loads(notebook)
     cell_iterator = 1
     for cell in parsed["cells"]:
         if cell["cell_type"] == "code":
             cell["execution_count"] = cell_iterator
+            for output in cell["outputs"]:
+                output["execution_count"] = cell_iterator
             cell_iterator += 1
     notebook.write_text(json.dumps(parsed, indent=2))
