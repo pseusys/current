1b92286bd79a9284ed9dd46075a2910fc2d16cbf
milesartemius <shveitsar215@gmail.com>
2021-11-12 01:14:30 +0300

    AES fixed

--- sem7/CS-lab5/AES.py
+++ sem7/CS-lab5/AES.py
@@ -2,11 +2,19 @@ import json
 from functools import reduce
 
 
+def hex_arr(lst):
+    return f"[{' '.join(hex(item) for item in lst)}]"
+
+
+def hex_mat(mat):
+    return "\n" + '\n'.join(f"[ {' '.join(hex(item) for item in mat[4 * j:4 * j + 4])} ]" for j in range(0, 4))
+
+
 def prepare_data_from_string(string):
     string = ((' ' * (16 - len(string))) if len(string) < 16 else '') + string
     print(f"Preparing string: '{string}'")
     bts = bytearray(string, 'ascii')[:16]
-    print(" ".join(string), " ".join(str(i) for i in bts), " ".join(format(i, '08b') for i in bts), sep='\n')
+    print(" ".join(string), " ".join(hex(i) for i in bts), " ".join(format(i, '08b') for i in bts), sep='\n')
     return [int(i) for i in bts]
 
 
@@ -39,20 +47,19 @@ def gpow(a, b):
 
 
 def round_key(num, prev_bytes):
-    print(f"\nGenerating round key for round {num}, using {prev_bytes} as previous key bytes...")
+    print(f"\nGenerating round key for round {num}, using {hex_arr(prev_bytes)} as previous key bytes...")
     rotated = rotate_bytes(prev_bytes[-4:], 1)
-    print(f"Rotated: {rotated}")
+    print(f"Rotated: {hex_arr(rotated)}")
     substituted = substitute(rotated)
-    print(f"Substituted: {substituted}")
-    xored = [substituted[0] ^ gpow(2, num)] + substituted[1:]
-    print(f"XORed with round constant ({gpow(2, num)}, 0, 0, 0): {xored}")
-    w4 = xor(prev_bytes[-4:], xored)
-    w5 = xor(w4, prev_bytes[-8:-4])
-    w6 = xor(w5, prev_bytes[-12:-8])
-    w7 = xor(w6, prev_bytes[-16:-12])
-    result = w4 + w5 + w6 + w7
-    print(f"{num}th round key: {result}")
-    return result
+    print(f"Substituted: {hex_arr(substituted)}")
+    rconst = [gpow(2, num), 0, 0, 0]
+    xored = xor(substituted, rconst)
+    print(f"XORed with round constant ({hex_arr(rconst)}): {hex_arr(xored)}")
+    w = xored
+    for i in range(-4, 1):
+        w += xor(w[-4:], prev_bytes[4 * i:4 * i + 4])
+    print(f"{num}th round key: {hex_arr(w[4:])}")
+    return w[4:]
 
 
 def compare(check, real):
@@ -63,7 +70,7 @@ def unsubstitute(bts):
     return list(map(lambda b: tables["sb-1"][16 * (b // 16) + (b % 16)], bts))
 
 
-# Визуализация алгоритма DES
+# Визуализация алгоритма AES
 if __name__ == '__main__':
     with open("AES_tables.json") as AES_tables:
         tables = json.load(AES_tables, object_hook=lambda x: dict(zip(x, map(lambda val: [int(v, 16) for v in val], x.values()))))
@@ -82,57 +89,59 @@ if __name__ == '__main__':
 
     # Шаг 2: добавление ключа
     matrix = xor(plain_text, key)
-    print(f"\n\nMatrix XORed with initial key: {matrix}\n")
+    print(f"\n\nMatrix XORed with initial key: {hex_mat(matrix)}\n")
 
     # Шаг 3: раундовые преобразования
     for i in range(0, 10):
+        print(f"\n\nRound: {i}")
+
         matrix = substitute(matrix)
-        print(f"\nBytes substituted: {matrix}")
+        print(f"\nBytes substituted: {hex_mat(matrix)}")
 
         for j in range(0, 4):
             matrix[j::4] = rotate_bytes(matrix[j::4], j)
-        print(f"Rows shifted: {matrix}")
+        print(f"\nRows shifted: {hex_mat(matrix)}")
 
         if i < 9:
             f = [0x02, 0x03, 0x01, 0x01]
             fixed = f + rotate_bytes(f, -1) + rotate_bytes(f, -2) + rotate_bytes(f, -3)
-            multiplied = matrix.copy()
+            multiplied = [None] * len(matrix)
             for j in range(0, len(matrix)):
-                mul = [gmul(m, f) for m, f in zip(matrix[4 * (j // 4):4 * (j // 4) + 4], fixed[(j % 4)::4],)]
-                multiplied[j] = mul[0] ^ mul[1] ^ mul[2] ^ mul[3]
+                multiplied[j] = reduce(lambda x, y: x ^ y, [gmul(m, f) for m, f in zip(matrix[j - (j % 4):j - (j % 4) + 4], fixed[4 * (j % 4):4 * (j % 4) + 4])])
             matrix = multiplied
-            print(f"Columns mixed: {matrix}")
+            print(f"\nColumns mixed: {hex_mat(matrix)}")
 
         matrix = xor(matrix, round_keys[i])
-        print(f"Key added: {matrix}")
+        print(f"\nKey added: {hex_mat(matrix)}")
 
     # Шаг 3: результат работы алгоритма
-    print(f"\n\nResult: {matrix}")
+    print(f"\n\nResult: {hex_mat(matrix)}")
 
     # Шаг 7: проверка
-    print(f"\n\nDecrypting {matrix}...")
+    print(f"\n\n\nDecrypting {matrix}...")
     for i in range(9, -1, -1):
+        print(f"\n\nRound: {9 - i}")
+
         matrix = xor(matrix, round_keys[i])
-        print(f"\nKey added: {matrix}")
+        print(f"\nKey added: {hex_mat(matrix)}")
 
         if i < 9:
             f = [0x0e, 0x0b, 0x0d, 0x09]
             fixed = f + rotate_bytes(f, -1) + rotate_bytes(f, -2) + rotate_bytes(f, -3)
-            multiplied = matrix.copy()
+            multiplied = [None] * len(matrix)
             for j in range(0, len(matrix)):
-                mul = [gmul(m, f) for m, f in zip(matrix[4 * (j // 4):4 * (j // 4) + 4], fixed[(j % 4)::4],)]
-                multiplied[j] = mul[0] ^ mul[1] ^ mul[2] ^ mul[3]
+                multiplied[j] = reduce(lambda x, y: x ^ y, [gmul(m, f) for m, f in zip(matrix[j - (j % 4):j - (j % 4) + 4], fixed[4 * (j % 4):4 * (j % 4) + 4])])
             matrix = multiplied
-            print(f"Columns mixed: {matrix}")
+            print(f"\nColumns mixed: {hex_mat(matrix)}")
 
         for j in range(0, 4):
             matrix[4 - j::4] = rotate_bytes(matrix[4 - j::4], j)
-        print(f"Rows shifted: {matrix}")
+        print(f"\nRows shifted: {hex_mat(matrix)}")
 
         matrix = unsubstitute(matrix)
-        print(f"Bytes substituted: {matrix}")
+        print(f"\nBytes substituted: {hex_mat(matrix)}")
 
     matrix = xor(matrix, key)
-    print(f"\n\nMatrix XORed with initial key: {matrix}\n")
+    print(f"\n\nMatrix XORed with initial key: {hex_mat(matrix)}\n")
 
-    print(f"Decoded string value: '{bytearray(matrix).decode('ascii')}'")
+    print(f"\nDecoded string value: '{bytearray(matrix).decode('ascii')}'")
