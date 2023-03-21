06ccb8b4ce84b2c07e32596d8bde2a766aa9f193
Pia314 <pia.doepper@web.de>
2023-03-21 22:52:37 +0100

    addind MPI_Wtime

--- sem10/PAP-lab3/sources/4_ping_pong.c
+++ sem10/PAP-lab3/sources/4_ping_pong.c
@@ -10,6 +10,8 @@ int comp (const void* elem1, const void* elem2) {
 int main(int argc, char** argv) {
     int rank, size, tag = 0;
     int array_size = 10;
+    double starttime, endtime;
+    double MPI_Wtime();
 
     MPI_Init(&argc,&argv);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
@@ -19,12 +21,15 @@ int main(int argc, char** argv) {
     if (rank == 0) {
         int recipient = 1;
         init_array_random_float(array, array_size);
+        starttime = MPI_Wtime();
         MPI_Send(array, array_size, MPI_DOUBLE, recipient, tag, MPI_COMM_WORLD);
         printf("Process %d sent array: ", rank + 1);
         print_array_float(array, array_size);
         MPI_Recv(array, array_size, MPI_DOUBLE, recipient, tag, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
         printf("Process %d received array: ", rank + 1);
+        endtime   = MPI_Wtime();
         print_array_float(array, array_size);
+        printf("That took %f seconds\n",endtime-starttime);
     } else {
         int recipient = 0;
         MPI_Recv(array, array_size, MPI_DOUBLE, recipient, tag, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
