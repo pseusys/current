8fc1fea7e68b1688bd9a420e7fad91edb1235f90
pseusys <shveitsar215@gmail.com>
2023-04-13 01:57:45 +0200

    PAP lab extra added

---
+++ sem10/PAP-lab-extra/src/general.c
@@ -0,0 +1,3 @@
+void evaluate_performance() {
+
+}
---
+++ sem10/PAP-lab-extra/src/utils.h
@@ -0,0 +1,29 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#ifndef EXPERIMENTS
+#define EXPERIMENTS 5
+#endif
+
+extern double experiments [];
+
+extern int perf_eval;
+extern int check_correctness;
+extern int rinit;
+
+double* allocMatrix(int width, int height);
+
+void initMatrix(int width, int height, double *A);
+void initMatrixZero(int width, int height, double *A);
+
+double* createMatrixCopy(int width, int height, double *A);
+
+void matrixMultiplicationReference(int width, int height, double *A, double *B, double *C);
+
+int checkMatricesEquality(int width, int height, double *V1, double *V2);
+
+void printMatrix(int width, int height, double *A);
+
+double average_time();
+
+#endif // UTILS_H
