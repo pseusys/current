6ae6f2cf3ea616bf21640df1580a3731bf7e8a92
pseusys <shveitsar215@gmail.com>
2023-04-22 16:36:22 +0200

    6 fix

--- sem10/PAP-lab4/exercise_6.c
+++ sem10/PAP-lab4/exercise_6.c
@@ -60,11 +60,11 @@ void lbm_comm_ghost_exchange_ex6(lbm_comm_t * comm, lbm_mesh_t * mesh)
 
 	if (communicate_right) {
 		MPI_Isend(send_right, comm->height * DIRECTIONS, MPI_DOUBLE, get_neighbor_rank(comm, 1, 0), tag, comm->communicator, comm->requests);
-		MPI_Irecv(recv_left, comm->height * DIRECTIONS, MPI_DOUBLE, get_neighbor_rank(comm, 1, 0), tag, comm->communicator, comm->requests);
+		MPI_Irecv(recv_left, comm->height * DIRECTIONS, MPI_DOUBLE, get_neighbor_rank(comm, 1, 0), tag, comm->communicator, comm->requests + 1);
 	}
 	if (communicate_left) {
-		MPI_Isend(send_left, comm->height * DIRECTIONS, MPI_DOUBLE, get_neighbor_rank(comm, -1, 0), tag, comm->communicator, comm->requests + 1);
-		MPI_Irecv(recv_right, comm->height * DIRECTIONS, MPI_DOUBLE, get_neighbor_rank(comm, -1, 0), tag, comm->communicator, comm->requests + 1);
+		MPI_Isend(send_left, comm->height * DIRECTIONS, MPI_DOUBLE, get_neighbor_rank(comm, -1, 0), tag, comm->communicator, comm->requests + 2);
+		MPI_Irecv(recv_right, comm->height * DIRECTIONS, MPI_DOUBLE, get_neighbor_rank(comm, -1, 0), tag, comm->communicator, comm->requests + 3);
 	}
 
 	// TOP <-> BOTTOM
@@ -78,13 +78,23 @@ void lbm_comm_ghost_exchange_ex6(lbm_comm_t * comm, lbm_mesh_t * mesh)
 	int communicate_down = comm->rank_y != 0;
 
 	if (communicate_up) {
-		MPI_Isend(send_down, 1, comm->type, get_neighbor_rank(comm, 0, 1), tag, comm->communicator, comm->requests + 2);
-		MPI_Irecv(recv_up, 1, comm->type, get_neighbor_rank(comm, 0, 1), tag, comm->communicator, comm->requests + 2);
+		MPI_Isend(send_down, 1, comm->type, get_neighbor_rank(comm, 0, 1), tag, comm->communicator, comm->requests + 4);
+		MPI_Irecv(recv_up, 1, comm->type, get_neighbor_rank(comm, 0, 1), tag, comm->communicator, comm->requests + 5);
 	}
 	if (communicate_down) {
-		MPI_Isend(send_up, 1, comm->type, get_neighbor_rank(comm, 0, -1), tag, comm->communicator, comm->requests + 3);
-		MPI_Irecv(recv_down, 1, comm->type, get_neighbor_rank(comm, 0, -1), tag, comm->communicator, comm->requests + 3);
+		MPI_Isend(send_up, 1, comm->type, get_neighbor_rank(comm, 0, -1), tag, comm->communicator, comm->requests + 6);
+		MPI_Irecv(recv_down, 1, comm->type, get_neighbor_rank(comm, 0, -1), tag, comm->communicator, comm->requests + 7);
 	}
+
+	// WAIT STRAIGHT COMMUNICATIONS
+
+	if (communicate_right && !communicate_left) MPI_Waitall(2, comm->requests, MPI_STATUS_IGNORE);
+	else if (communicate_left && !communicate_right) MPI_Waitall(2, comm->requests + 2, MPI_STATUS_IGNORE);
+	else if (communicate_right && communicate_left) MPI_Waitall(4, comm->requests, MPI_STATUSES_IGNORE);
+
+	if (communicate_up && !communicate_down) MPI_Waitall(2, comm->requests + 4, MPI_STATUS_IGNORE);
+	else if (communicate_down && !communicate_up) MPI_Waitall(2, comm->requests + 6, MPI_STATUS_IGNORE);
+	else if (communicate_up && communicate_down) MPI_Waitall(4, comm->requests + 4, MPI_STATUSES_IGNORE);
 
 	// DIAGONALS (TOP LEFT) <-> DIAGONALS (BOTTOM RIGHT)
 
@@ -92,12 +102,12 @@ void lbm_comm_ghost_exchange_ex6(lbm_comm_t * comm, lbm_mesh_t * mesh)
 	int communicate_right_down = comm->rank_x != 0 && comm->rank_y != 0;
 
 	if (communicate_left_up) {
-		MPI_Isend(lbm_mesh_get_cell(mesh, comm->width - 2, comm->height - 2), DIRECTIONS, MPI_DOUBLE, get_neighbor_rank(comm, 1, 1), tag, comm->communicator, comm->requests + 4);
-		MPI_Irecv(lbm_mesh_get_cell(mesh, comm->width - 1, comm->height - 1), DIRECTIONS, MPI_DOUBLE, get_neighbor_rank(comm, 1, 1), tag, comm->communicator, comm->requests + 4);
+		MPI_Isend(lbm_mesh_get_cell(mesh, comm->width - 2, comm->height - 2), DIRECTIONS, MPI_DOUBLE, get_neighbor_rank(comm, 1, 1), tag, comm->communicator, comm->requests + 8);
+		MPI_Irecv(lbm_mesh_get_cell(mesh, comm->width - 1, comm->height - 1), DIRECTIONS, MPI_DOUBLE, get_neighbor_rank(comm, 1, 1), tag, comm->communicator, comm->requests + 9);
 	}
 	if (communicate_right_down) {
-		MPI_Isend(lbm_mesh_get_cell(mesh, 1, 1), DIRECTIONS, MPI_DOUBLE, get_neighbor_rank(comm, -1, -1), tag, comm->communicator, comm->requests + 5);
-		MPI_Irecv(lbm_mesh_get_cell(mesh, 0, 0), DIRECTIONS, MPI_DOUBLE, get_neighbor_rank(comm, -1, -1), tag, comm->communicator, comm->requests + 5);
+		MPI_Isend(lbm_mesh_get_cell(mesh, 1, 1), DIRECTIONS, MPI_DOUBLE, get_neighbor_rank(comm, -1, -1), tag, comm->communicator, comm->requests + 10);
+		MPI_Irecv(lbm_mesh_get_cell(mesh, 0, 0), DIRECTIONS, MPI_DOUBLE, get_neighbor_rank(comm, -1, -1), tag, comm->communicator, comm->requests + 11);
 	}
 
 	// DIAGONALS (BOTTOM LEFT) <-> DIAGONALS (TOP RIGHT)
@@ -106,29 +116,21 @@ void lbm_comm_ghost_exchange_ex6(lbm_comm_t * comm, lbm_mesh_t * mesh)
 	int communicate_right_up = comm->rank_x != 0 && comm->rank_y != comm->nb_y - 1;
 
 	if (communicate_left_down) {
-		MPI_Isend(lbm_mesh_get_cell(mesh, comm->width - 2, 1), DIRECTIONS, MPI_DOUBLE, get_neighbor_rank(comm, 1, -1), tag, comm->communicator, comm->requests + 6);
-		MPI_Irecv(lbm_mesh_get_cell(mesh, comm->width - 1, 0), DIRECTIONS, MPI_DOUBLE, get_neighbor_rank(comm, 1, -1), tag, comm->communicator, comm->requests + 6);
+		MPI_Isend(lbm_mesh_get_cell(mesh, comm->width - 2, 1), DIRECTIONS, MPI_DOUBLE, get_neighbor_rank(comm, 1, -1), tag, comm->communicator, comm->requests + 12);
+		MPI_Irecv(lbm_mesh_get_cell(mesh, comm->width - 1, 0), DIRECTIONS, MPI_DOUBLE, get_neighbor_rank(comm, 1, -1), tag, comm->communicator, comm->requests + 13);
 	}
 	if (communicate_right_up) {
-		MPI_Isend(lbm_mesh_get_cell(mesh, 1, comm->height - 2), DIRECTIONS, MPI_DOUBLE, get_neighbor_rank(comm, -1, 1), tag, comm->communicator, comm->requests + 7);
-		MPI_Irecv(lbm_mesh_get_cell(mesh, 0, comm->height - 1), DIRECTIONS, MPI_DOUBLE, get_neighbor_rank(comm, -1, 1), tag, comm->communicator, comm->requests + 7);
+		MPI_Isend(lbm_mesh_get_cell(mesh, 1, comm->height - 2), DIRECTIONS, MPI_DOUBLE, get_neighbor_rank(comm, -1, 1), tag, comm->communicator, comm->requests + 14);
+		MPI_Irecv(lbm_mesh_get_cell(mesh, 0, comm->height - 1), DIRECTIONS, MPI_DOUBLE, get_neighbor_rank(comm, -1, 1), tag, comm->communicator, comm->requests + 15);
 	}
 
-	// WAIT ALL
+	// WAIT DIAGONAL COMMUNICATIONS
 
-	if (communicate_right && !communicate_left) MPI_Wait(comm->requests, MPI_STATUS_IGNORE);
-	else if (communicate_left && !communicate_right) MPI_Wait(comm->requests + 1, MPI_STATUS_IGNORE);
-	else if (communicate_right && communicate_left) MPI_Waitall(2, comm->requests, MPI_STATUSES_IGNORE);
+	if (communicate_left_up && !communicate_right_down) MPI_Waitall(2, comm->requests + 8, MPI_STATUS_IGNORE);
+	else if (communicate_right_down && !communicate_left_up) MPI_Waitall(2, comm->requests + 10, MPI_STATUS_IGNORE);
+	else if (communicate_left_up && communicate_right_down) MPI_Waitall(4, comm->requests + 8, MPI_STATUSES_IGNORE);
 
-	if (communicate_up && !communicate_down) MPI_Wait(comm->requests + 2, MPI_STATUS_IGNORE);
-	else if (communicate_down && !communicate_up) MPI_Wait(comm->requests + 3, MPI_STATUS_IGNORE);
-	else if (communicate_up && communicate_down) MPI_Waitall(2, comm->requests + 2, MPI_STATUSES_IGNORE);
-
-	if (communicate_left_up && !communicate_right_down) MPI_Wait(comm->requests + 4, MPI_STATUS_IGNORE);
-	else if (communicate_right_down && !communicate_left_up) MPI_Wait(comm->requests + 5, MPI_STATUS_IGNORE);
-	else if (communicate_left_up && communicate_right_down) MPI_Waitall(2, comm->requests + 4, MPI_STATUSES_IGNORE);
-
-	if (communicate_left_down && !communicate_right_up) MPI_Wait(comm->requests + 6, MPI_STATUS_IGNORE);
-	else if (communicate_right_up && !communicate_left_down) MPI_Wait(comm->requests + 7, MPI_STATUS_IGNORE);
-	else if (communicate_left_down && communicate_right_up) MPI_Waitall(2, comm->requests + 6, MPI_STATUSES_IGNORE);
+	if (communicate_left_down && !communicate_right_up) MPI_Waitall(2, comm->requests + 12, MPI_STATUS_IGNORE);
+	else if (communicate_right_up && !communicate_left_down) MPI_Waitall(2, comm->requests + 14, MPI_STATUS_IGNORE);
+	else if (communicate_left_down && communicate_right_up) MPI_Waitall(4, comm->requests + 12, MPI_STATUSES_IGNORE);
 }
