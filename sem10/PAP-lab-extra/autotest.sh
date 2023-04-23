92b1a923e6b37d593738ddab5e05af96f3e48e6e
pseusys <shveitsar215@gmail.com>
2023-04-23 13:55:12 +0200

    autotest altered

--- sem10/PAP-lab-extra/autotest.sh
+++ sem10/PAP-lab-extra/autotest.sh
@@ -7,22 +7,20 @@ function test_matrix_size {
 
     echo -n "matrix size;" >> $1
     for i in "${ALGORITHMS[@]}"; do
-        echo -n "$i;" 
+        echo -n "$i;" >> $1
     done
     echo >> $1
 
-    for i in {0..10}; do
-        MT="$((2^$i))"
+    TH=$(nproc --all)
+    for i in {2..15}; do
+        MT="$((2 ** $i))"
 
         echo -n "$MT;" >> $1
         EARLY_BREAK=0
         for ((a=0;a<ALGORITHMS_NUM;a++)); do
-            if [ $a == 0 ]; then TH=1;
-            else TH=$(nproc --all); fi
-
-            echo -e "\ttesting with: THREADS=$TH EXERCISE=$a MATRIX=${MT}) ITERATIONS=16000"
+            echo -e "\ttesting with: THREADS=$TH ALGO=${ALGORITHMS[$a]} MATRIX=${MT}"
             START=`date +%s%N`
-            timeout --preserve-status ${TIMEOUT}s bash -c "mpirun -np $TH --oversubscribe ./build/$a.run ${MT}" &> /dev/null && {
+            timeout --preserve-status ${TIMEOUT}s bash -c "mpirun -np $TH --oversubscribe ./build/${ALGORITHMS[$a]}.run $MT" &> /dev/null && {
                 END=`date +%s%N`
                 DURATION="$(($END - $START))"
                 echo -n "$DURATION;" >> $1
@@ -36,7 +34,6 @@ function test_matrix_size {
         if [[ $EARLY_BREAK == $ALGORITHMS_NUM ]]; then break; fi
     done
     echo >> $1
-
 }
 
 function test_threads_number {
@@ -50,16 +47,13 @@ function test_threads_number {
     done
     echo >> $1
 
-    for i in {0..15}; do
-        TH="$((2 * $i))"
-        if [ $i == 0 ]; then TH=1; fi
-
-        echo -n "$TH;" >> $1
+    for i in {1..20}; do
+        echo -n "$i;" >> $1
         EARLY_BREAK=0
         for ((a=0;a<ALGORITHMS_NUM;a++)); do
-            echo -e "\ttesting with: THREADS=$TH ALGO=$a MATRIX=50x50 ITERATIONS=16000"
+            echo -e "\ttesting with: THREADS=$i ALGO=${ALGORITHMS[$a]} MATRIX=40"
             START=`date +%s%N`
-            timeout --preserve-status ${TIMEOUT}s bash -c "mpirun -np $TH --oversubscribe ./build/$a.run 50 " &> /dev/null && {
+            timeout --preserve-status ${TIMEOUT}s bash -c "mpirun -np $i --oversubscribe ./build/${ALGORITHMS[$a]}.run 40" &> /dev/null && {
                 END=`date +%s%N`
                 DURATION="$(($END - $START))"
                 echo -n "$DURATION;" >> $1
@@ -76,14 +70,14 @@ function test_threads_number {
 }
 
 
-ALGORITHMS=(sequential matrix-vector matrix-matrix)
+ALGORITHMS=("sequential" "matrix-vector" "matrix-matrix")
 
-echo -e "Testing with fixed thread number, different matrix size and fixed iterations number..."
+echo -e "Testing with fixed thread number and different matrix size..."
 rm -f test/matrix_size.csv 2> /dev/null
 test_matrix_size "test/matrix_size.csv"
-echo -e "Tested with fixed thread number, different matrix size and fixed iterations number!"
+echo -e "Tested with fixed thread number and different matrix size!"
 
-echo -e "Testing with different thread number, fixed matrix size and iterations number..."
+echo -e "Testing with different thread number and fixed matrix size..."
 rm -f test/threads_number.csv 2> /dev/null
 test_threads_number "test/threads_number.csv"
-echo -e "Tested with different thread number, fixed matrix size and iterations number!"
+echo -e "Tested with different thread number and fixed matrix size!"
