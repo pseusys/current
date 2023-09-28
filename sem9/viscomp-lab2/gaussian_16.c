2c489eb209bea39f169cd2acb69a2eea12eb64f6
pseusys <shveitsar215@gmail.com>
2023-09-28 19:31:54 +0200

    different filters added

--- sem9/viscomp-lab2/gaussian_16.c
+++ sem9/viscomp-lab2/gaussian_16.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
 #include <math.h>
 
@@ -21,7 +22,7 @@ long long int gaussian(int dimension, long long int* matrix) {
     return sum;
 }
 
-long long int filter_one(int x, int y, int width, int dimension, long long int* gaussian, byte* source) {
+long long int filter_one_gaussian(int x, int y, int width, int dimension, long long int* gaussian, byte* source) {
     int dimstep = (dimension - 1) / 2;
     long long int result = 0;
     for (int i = -dimstep; i <= dimstep; i++)
@@ -30,12 +31,12 @@ long long int filter_one(int x, int y, int width, int dimension, long long int* 
     return result;
 }
 
-void filter(int width, int height, int ntimes, int dimension, byte* source) {
+void filter_all_gaussian(int width, int height, int ntimes, int dimension, byte* source) {
     long long int* matrix = (long long int*) malloc(dimension * dimension * sizeof(long long int));
     long long int gaussian_sum = gaussian(dimension, matrix);
 
     int dimstep = (dimension - 1) / 2;
-    byte *bytepadded = (byte*) calloc((width + dimstep * 2) * (height + dimstep * 2), sizeof(byte));
+    byte* bytepadded = (byte*) calloc((width + dimstep * 2) * (height + dimstep * 2), sizeof(byte));
 
     for (int i = 0; i < ntimes; i++) {
         for (int i = dimstep; i < height + dimstep; i++)
@@ -43,27 +44,93 @@ void filter(int width, int height, int ntimes, int dimension, byte* source) {
                 bytepadded[i * width + j] = source[(i - dimstep) * width + j - dimstep];
         for (int i = dimstep; i < height + dimstep; i++)
             for (int j = dimstep; j < width + dimstep; j++)
-                source[(i - dimstep) * width + j - dimstep] = round(filter_one(i, j, width, dimension, matrix, bytepadded) / gaussian_sum);
+                source[(i - dimstep) * width + j - dimstep] = round(filter_one_gaussian(i, j, width, dimension, matrix, bytepadded) / gaussian_sum);
     }
 
     free(bytepadded);
     free(matrix);
 }
 
+int average_comparator(const void* a, const void* b) {
+   return *((byte*) a) - *((byte*) b);
+}
+
+byte filter_one_average(int x, int y, int width, int dimension, byte* source) {
+    int dimstep = (dimension - 1) / 2;
+    long long int average_sum = dimension * dimension;
+    byte* data = (byte*) calloc(average_sum, sizeof(byte));
+
+    int index = 0;
+    for (int i = -dimstep; i <= dimstep; i++)
+        for (int j = -dimstep; j <= dimstep; j++) {
+            data[index] = source[(x + i) * width + y + i];
+            index++;
+        }
+
+    qsort(data, average_sum, sizeof(byte), average_comparator);
+    return data[average_sum / 2];
+}
+
+void filter_all_average(int width, int height, int ntimes, int dimension, byte* source) {
+    int dimstep = (dimension - 1) / 2;
+    byte* bytepadded = (byte*) calloc((width + dimstep * 2) * (height + dimstep * 2), sizeof(byte));
+
+    for (int i = 0; i < ntimes; i++) {
+        for (int i = dimstep; i < height + dimstep; i++)
+            for (int j = dimstep; j < width + dimstep; j++)
+                bytepadded[i * width + j] = source[(i - dimstep) * width + j - dimstep];
+        for (int i = dimstep; i < height + dimstep; i++)
+            for (int j = dimstep; j < width + dimstep; j++)
+                source[(i - dimstep) * width + j - dimstep] = filter_one_average(i, j, width, dimension, bytepadded);
+    }
+
+    free(bytepadded);
+}
+
+byte filter_one_mean(int x, int y, int width, int dimension, byte* source) {
+    int dimstep = (dimension - 1) / 2;
+
+    long long int data_sum = 0;
+    for (int i = -dimstep; i <= dimstep; i++)
+        for (int j = -dimstep; j <= dimstep; j++)
+            data_sum += source[(x + i) * width + y + i];
+
+    return data_sum / (dimension * dimension);
+}
+
+void filter_all_mean(int width, int height, int ntimes, int dimension, byte* source) {
+    int dimstep = (dimension - 1) / 2;
+    byte* bytepadded = (byte*) calloc((width + dimstep * 2) * (height + dimstep * 2), sizeof(byte));
+
+    for (int i = 0; i < ntimes; i++) {
+        for (int i = dimstep; i < height + dimstep; i++)
+            for (int j = dimstep; j < width + dimstep; j++)
+                bytepadded[i * width + j] = source[(i - dimstep) * width + j - dimstep];
+        for (int i = dimstep; i < height + dimstep; i++)
+            for (int j = dimstep; j < width + dimstep; j++)
+                source[(i - dimstep) * width + j - dimstep] = filter_one_mean(i, j, width, dimension, bytepadded);
+    }
+
+    free(bytepadded);
+}
+
 int main(int argc, char* argv[]) {
-    FILE *ifp, *ofp;
-    byte *bytemap;
+    FILE* ifp, * ofp;
+    byte* bytemap;
+    char* filter_type;
     int version, rows, cols, ntimes, dimension, maxval = 255;
 
     /* Arguments */
-    if (argc != 5) {
+    if (argc != 6) {
         printf("\nUsage: %s file_in file_out \n\n", argv[0]);
         exit(0);
     }
 
-    ntimes = atoi(argv[3]);
+    filter_type = argv[3];
 
-    dimension = atoi(argv[4]);
+    ntimes = atoi(argv[4]);
+
+    dimension = atoi(argv[5]);
     if (dimension < 0 || dimension % 2 == 0) {
         printf("Filter dimension should be a positive odd number\n");
         exit(1);
@@ -98,7 +165,13 @@ int main(int argc, char* argv[]) {
     /* Closing input file */
     fclose(ifp);
 
-    filter(cols, rows, ntimes, dimension, bytemap);
+    if (strcmp(filter_type, "gaussian") == 0) filter_all_gaussian(cols, rows, ntimes, dimension, bytemap);
+    else if (strcmp(filter_type, "average") == 0) filter_all_average(cols, rows, ntimes, dimension, bytemap);
+    else if (strcmp(filter_type, "mean") == 0) filter_all_mean(cols, rows, ntimes, dimension, bytemap);
+    else {
+        printf("Unknown filtering type: %s\n", filter_type);
+        exit(1);
+    }
 
     /* Opening output file */
     ofp = fopen(argv[2], "w");
