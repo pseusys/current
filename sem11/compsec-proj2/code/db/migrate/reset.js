8af9d287983718f295cfca1ea9cb74e6c392b9b5
pseusys <shveitsar215@gmail.com>
2024-11-05 15:40:45 +0800

    prettier :)

--- sem11/compsec-proj2/code/db/migrate/reset.js
+++ sem11/compsec-proj2/code/db/migrate/reset.js
@@ -1,7 +1,7 @@
-import sqlite from 'sqlite';
+import sqlite from "sqlite";
 
-const db = sqlite.open('./db/database.sqlite').then(db => {
-  db.migrate({migrationsPath:'./db/migrate/'})
+const db = sqlite.open("./db/database.sqlite").then((db) => {
+	db.migrate({ migrationsPath: "./db/migrate/" });
 });
 
 export default db;
